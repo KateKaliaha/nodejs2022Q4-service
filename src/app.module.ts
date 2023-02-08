a7b7df9ace6e88a9be7730a4b40b06a07e84d712
Katherina Kaliaha <mellinda077@mail.ru>
2023-02-08 22:22:15 +0300

    init: start task Home library service task 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DBModule } from './DB/db.module';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
@@ -17,7 +15,7 @@ import { FavsModule } from './favs/favs.module';
     AlbumsModule,
     FavsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
